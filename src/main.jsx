275f6df0f2c9bf955961c82eb5759bdc00063041
Md Enjamul Islam <mdenjamulislam527@gmail.com>
2024-03-05 14:09:57 +0600

    Setup folder structure and router

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import router from "./routers/Routers";
+import { RouterProvider } from "react-router-dom";
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <div>Home</div>,
-    },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
