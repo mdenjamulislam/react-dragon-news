a78dc1a014a0405443ed239b948e232b76fcfb3f
Md Enjamul Islam <mdenjamulislam527@gmail.com>
2024-03-06 14:52:41 +0600

    Dynamic date time added with header

--- src/pages/shared/Header.jsx
+++ src/pages/shared/Header.jsx
@@ -1,8 +1,17 @@
-
+import moment from "moment";
+import Logo from "../../assets/images/logo.png";
 const Header = () => {
     return (
-        <div>
-            header
+        <div className="flex flex-col items-center justify-center space-y-5">
+            <div>
+                <img src={Logo} alt="Dragon News" className="h-auto w-auto" />
+            </div>
+            <div className="space-y-2 text-center">
+                <p>Journalism Without Fear or Favour</p>
+                <p className="font-medium">
+                    {moment().format("dddd, MMMM, YYYY")}
+                </p>
+            </div>
         </div>
     );
 };
