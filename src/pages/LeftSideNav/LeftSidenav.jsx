209037d436d472c5847ae529e346ad07a5a372f5
Md Enjamul Islam <mdenjamulislam527@gmail.com>
2024-03-06 17:23:19 +0600

    Category Added

--- src/pages/LeftSideNav/LeftSidenav.jsx
+++ src/pages/LeftSideNav/LeftSidenav.jsx
@@ -1,9 +1,21 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 
 const LeftSidenav = () => {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        fetch('categories.json')
+            .then(res => res.json())
+            .then(data => setCategories(data))
+    }, []);
     return (
         <div>
-           left side 
+            <h2 className="heading--two mb-5">All Category</h2> 
+            {
+                categories.map(category => <Link to={`/category/${category.id}`} key={category.id} className="block px-5 py-2 font-medium hover:bg-gray-200 hover:text-primary active:bg-gray-200">{ category.name}</Link>)
+            }
         </div>
     );
 };
